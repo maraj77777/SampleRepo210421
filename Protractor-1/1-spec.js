5d6346769d2e9b17a140eb9996e831c66b9dc36c
Raj <maraj7009@gmail.com>
2021-04-23 12:07:50 +0530

    added a line on 23 april 2021 12:04 pm
    Signed-off-by: Raj <maraj7009@gmail.com>

--- Protractor-1/1-spec.js
+++ Protractor-1/1-spec.js
@@ -5,6 +5,7 @@ browser.ignoreSynchronization = true;
 browser.get('https://www.softwaretestingmaterial.com/');
 browser.driver.getTitle().then(function(pageTitle) {
 expect(pageTitle).toEqual('Software Testing Material');
+//testing on 23 april 2021 12:04 pm
 });
 });
 });
